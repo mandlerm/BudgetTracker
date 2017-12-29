49d0b552aa8fd5d852bf93ae3e15eeee7d2f4886
mandlerm <amaya.melissa@gmail.com>
2017-12-29 17:18:14 -0600

    shell of budget form in partial, passing in value of @budget

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -1,2 +1,22 @@
 class BudgetController < ApplicationController
+
+  def new
+    @budget = Budget.new
+  end
+
+  def create
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  private
+
+  def budget_params
+    params.require(:budget).permit()
+  end
 end
