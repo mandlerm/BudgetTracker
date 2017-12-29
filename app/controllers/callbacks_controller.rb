96ced394ecfca413163bc76a04581367afb3a833
mandlerm <amaya.melissa@gmail.com>
2017-12-29 08:58:13 -0600

    not sure what is going on with resets and reverts

---
+++ app/controllers/callbacks_controller.rb
@@ -0,0 +1,19 @@
+class CallbacksController < Devise::OmniauthCallbacksController
+
+  def google
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      sign_in_and_redirect @user
+  end
+
+  def github
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      sign_in_and_redirect @user
+  end
+
+  def facebook
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      sign_in_and_redirect @user
+  end
+
+
+end
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,13 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
+end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+end
+#resources said to add this line, but this changed error from csf token to invalid credientials
+  #,provider_ignores_state: true
+
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+end
