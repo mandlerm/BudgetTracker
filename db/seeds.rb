cbef9a42fea3957a7d1817a13db81fc6e83c7edc
mandlerm <amaya.melissa@gmail.com>
2017-12-29 16:20:32 -0600

    seed data seeded.  associations seem to be working correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,19 +47,18 @@ Income.create(
     budget_id: 1
 )
 
-
 IncomeSource.create(
-  name: "Job"
-  income_id: 1 
+  name: "Job",
+  income_id: 1
 )
 
 10.times do
   Outgo.create(
-    date: Faker::Time.between(DateTime.now - 29, DateTime.now-1)
-    amount: Faker::Number.between(1, 35)
-    location: Faker::Job.field
-    category_id:Faker::Number.between(1,10)
-    budget_id: 1
+    date: Faker::Time.between(DateTime.now - 29, DateTime.now-1),
+    amount: Faker::Number.between(1, 35),
+    location: Faker::Job.field,
+    category_id:Faker::Number.between(1,10),
+    budget_id: 1,
   )
   Category.create(title: Faker::Commerce.department)
 end
