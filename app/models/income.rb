734548a35047765dbde330c8237ed0fe127fb567
mandlerm <amaya.melissa@gmail.com>
2017-12-30 22:47:29 -0600

    create income controller

--- app/models/income.rb
+++ app/models/income.rb
@@ -2,4 +2,17 @@ class Income < ApplicationRecord
   has_many :income_sources
   has_many :transactions
   has_one :user, :through => :transactions
+
+  def create
+
+  end
+
+
+
+
+  private
+
+  def income_params
+    params.require(:income).permit(  )
+  end
 end
